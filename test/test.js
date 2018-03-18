034d3d03d1c378cdf6bb8c709dcbd22dc21c8ca9
Michael Shick <m@shick.us>
2018-03-17 17:20:54 -0700

    make the silly test pass

--- test/test.js
+++ test/test.js
@@ -6,9 +6,20 @@ const {POSTGRES_USER, POSTGRES_DB} = process.env;
 const pgConfig = `postgresql://${POSTGRES_USER}@localhost/${POSTGRES_DB}`;
 
 test('simple test', async t => {
-  const server = hapi.server({port: 5000});
+  try {
+    const server = hapi.server({port: 5000});
 
-  await server.register(plugin, {pgConfig});
+    await server.register({
+      plugin,
+      options: {
+        pgConfig
+      }
+    });
 
-  await server.start();
+    await server.start();
+
+    t.pass();
+  } catch (err) {
+    t.fail(err);
+  }
 });
